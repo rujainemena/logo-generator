6987be585f328b692736fca93c16ab0648e4caac
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-08-23 14:38:50 -0400

    corrected code in Circle.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// code for application
+const Circle = require("./lib/Circle");
+const Square = require("./lib/Square");
+const Triangle = require("./lib/Triangle");
+const inquirer = require("inquirer");
+const fs = require("fs");
